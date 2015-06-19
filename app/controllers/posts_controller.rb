394c9d4340121b4f6a0d4d57981b94abbd312962
及川秀明 <ym949pgd72f@gmail.com>
2015-06-19 20:29:00 +0900

    記事の編集機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,13 +13,26 @@ class PostsController < ApplicationController
         redirect_to "/posts/#{@post.id}"
   end
 
-def show
-  @post = Post.find(params[:id])
-end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-def index
-  @posts = Post.all
-end
+  def index
+   @posts = Post.all
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    @post.update_attributes(post_params)
+
+    redirect_to"/posts/#{@post.id}"
+
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
 
   get '/posts/new' => 'posts#new'
   post '/posts/' => 'posts#create'
+
   get '/posts/:id' => 'posts#show'
+
   get '/posts/' => 'posts#index'
+
+  get '/posts/:id/edit' => 'posts#edit'
+  patch '/post/:id' => 'posts#update'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
