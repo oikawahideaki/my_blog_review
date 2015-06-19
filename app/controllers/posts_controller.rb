2f4028698bc7fbb7011e57a79a45ecb0ef3a6b3f
及川秀明 <ym949pgd72f@gmail.com>
2015-06-19 19:02:28 +0900

    新規記事投稿機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+        @post.save
+
+        #↓あとでやる
+        redirect_to "/posts/#{@post.id}"
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(
+        :title,
+        :body,
+        :category
+        )
+
+    end
+
+
 end
