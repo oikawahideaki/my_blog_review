68da6032dea6e4bda37b0dfbc7763c694e2e2481
及川秀明 <ym949pgd72f@gmail.com>
2015-06-19 19:26:29 +0900

    記事の詳細機能を表示する機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,12 @@ class PostsController < ApplicationController
         redirect_to "/posts/#{@post.id}"
   end
 
+def show
+  @post = Post.find(params[:id])
+end
+
+
+
   private
 
     def post_params
