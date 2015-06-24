61fd408064e71534291df87b1b8cb39a4fbbf9cd
及川秀明 <ym949pgd72f@gmail.com>
2015-06-24 11:20:23 +0900

    category_idをテーブルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623161109) do
+ActiveRecord::Schema.define(version: 20150624015849) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20150623161109) do
     t.string   "title"
     t.text     "body"
     t.string   "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "category_id", default: 1, null: false
   end
 
 end
